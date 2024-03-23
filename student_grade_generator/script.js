4e639a6ca7db7399c587d2cbbb1e669ccd22ef65
tracychemtai <tracychemtai@gmail.com>
2024-03-23 12:21:15 +0300

    add grade generator

---
+++ student_grade_generator/script.js
@@ -0,0 +1,20 @@
+function getGrade(marks) {
+    marks = parseInt(document.getElementById('marks').value, 10) || 0;
+
+if (marks < 0 || marks >100) {
+alert( 'Enter a valid marks between 0 and 100.');
+return;
+}
+
+let grade =
+
+marks > 79 ? 'A' :
+marks >= 60 ? 'B' :
+marks >= 50 ? 'C' :
+marks >= 40 ? 'D' :
+'E';
+  console.log (grade)
+
+    
+    document.getElementById('results').innerText = `Your Grade: ${marks} is: ${grade}` 
+}
